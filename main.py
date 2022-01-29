f51e04a7de989ff2d615469792b78032f250b99a
Y.Hisaki <yhisaki31@gmail.com>
2022-01-29 23:29:03 +0900

    fix a bug of argument

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--policy", default="TD3")  # Policy name (TD3, DDPG or OurDDPG)
-    parser.add_argument("--env", default="HalfCheetah-v3")  # OpenAI gym environment name
+    parser.add_argument("--env", default="Walker2d-v3")  # OpenAI gym environment name
     parser.add_argument("--seed", default=0, type=int)  # Sets Gym, PyTorch and Numpy seeds
     parser.add_argument(
         "--start_timesteps", default=25e3, type=int
@@ -47,16 +47,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max_timesteps", default=1e6, type=int
     )  # Max time steps to run environment
-    parser.add_argument("--expl_noise", default=0.1)  # Std of Gaussian exploration noise
+    parser.add_argument(
+        "--expl_noise", type=float, default=0.1
+    )  # Std of Gaussian exploration noise
     parser.add_argument(
         "--batch_size", default=256, type=int
     )  # Batch size for both actor and critic
-    parser.add_argument("--discount", default=0.99)  # Discount factor
-    parser.add_argument("--tau", default=0.005)  # Target network update rate
+    parser.add_argument("--discount", type=float, default=0.99)  # Discount factor
+    parser.add_argument("--tau", type=float, default=0.005)  # Target network update rate
     parser.add_argument(
-        "--policy_noise", default=0.2
+        "--policy_noise", type=float, default=0.2
     )  # Noise added to target policy during critic update
-    parser.add_argument("--noise_clip", default=0.5)  # Range to clip target policy noise
+    parser.add_argument(
+        "--noise_clip", type=float, default=0.5
+    )  # Range to clip target policy noise
     parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
     parser.add_argument("--save_model", action="store_true")  # Save model and optimizer parameters
     parser.add_argument("--load_model", default="")  # Model load wandb run_id, "" doesn't load
